8937db554fe04eff51f5bcc61de9f1d7bc67d17a
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-03-18 17:00:08 -0400

    adding the skeletons of the webpage

---
+++ assets/js/questions.js
@@ -0,0 +1,37 @@
+var questions = [
+ {
+     title: "Which type of JavaScript language is ____?",
+     choices: ["Object-Oriented", "Object-Based", "Assembly-language", "High-level" ],
+     answer: "Object-Based",
+ },
+
+ {
+     title: "Which one of the following also known as Conditional Expression: ___",
+     choices: ["Alternative to if-else", "Switch statement", "If-then-else statement", "immediate if"],
+     answer: "immediate if",
+ },
+
+ {
+    title: "HTML stands for ___",
+    choices: ["HighText Machine Language", "HyperText and links Markup Language", "HyperText Markup Language", "None of these"],
+    answer: "HyperText Markup Language",
+},
+
+{
+    title: "Which of the following element is responsible for making the text bold in HTML?",
+    choices: ["<pre>","<a>", "<b>", "<br>"],
+    answer: "<b>",
+},
+
+{
+    title: "Which of the following is the correct syntax for referring the external style sheet?",
+    choices: ["<style src = example.css>", "<style src = 'example.css'>", "<stylesheet> example.css </stylesheet>", "<link rel='stylesheet' type='text/css' href='example.css'>"],
+    answer: "<link rel='stylesheet' type='text/css' href='example.css'>",
+},
+
+{
+    title: "The property in CSS used to change the background color of an element is ___",
+    choices: ["bgcolor", "color", "background-color", "All of the above"],
+    answer: "background-color",
+}
+];

f11148c4777ed1eade2211fee09dc11eec2c043f
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-03-19 18:55:53 -0400

    adding the algorithms of validating the right/wrong answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,15 @@ var startButton = document.querySelector("#start-btn");
 var timerEl = document.querySelector("#countdown");
 var submitBtn = document.querySelector("#submit");
 var startScreen = document.getElementById("quiz-questions");
+var correctScreen = document.getElementById("correct");
+var wrongScreen = document.getElementById("incorrect");
 var questionTitle = document.getElementById("question-title");
 var choice0 = document.getElementById("choice0");
 var choice1 = document.getElementById("choice1");
 var choice2 = document.getElementById("choice2");
 var choice3 = document.getElementById("choice3");
 var endScreen = document.getElementById("endMsg");
+var nextBtn = document.getElementById("nextQ");
 
 //Quiz variables
 let time = questions.length * 20; 
@@ -18,19 +21,25 @@ console.log(time);
 let questionNumber=0;
 let score = 0; 
 var answer=[];
+var chosen=[];
 var questionIndex = 0;
 
+for(let i=0; i < questions.length; i++) {
+    answer.push(questions[i].answer);
+ }
+ console.log(answer);
 
 function startQuiz() {
     //1. hide the start screen
     startScreen.classList.add("hide");
     //2. unhide the quiz question
     questionsEl.classList.remove("hide");
-    //3. for loop to display each question in turn, Index[0]
-    displayTest()
+    //3. display each question in turn, Index[0] & the next button
+    nextBtn.classList.remove("hide");
+    displayTest();
 }
 //no need ()
-startButton. addEventListener ("click", startQuiz)
+startButton.addEventListener ("click", startQuiz);
 
 function displayTest(){
     //1. grab the question-title element and insert current question tile
@@ -39,29 +48,89 @@ function displayTest(){
     choice1.innerHTML = "B. "+ questions[questionIndex].choices[1];
     choice2.innerHTML = "C. " + questions[questionIndex].choices[2];
     choice3.innerHTML = "D. " + questions[questionIndex].choices[3];
+    nextBtn.innerHTML = "Next Question";
 
 }
 
 function nextQuestion () {
-    
+    //enter the next question
+    if(!wrongScreen.classList.contains("hide")){
+        wrongScreen.classList.add("hide");
+    } 
+    if(!correctScreen.classList.contains("hide")){
+        correctScreen.classList.add("hide");
+    }
     questionIndex ++;
     if(questionIndex > questions.length - 1) {
         alert("You are done.");
         questionsEl.classList.add("hide");
         endScreen.classList.remove("hide");
         return;
-
     }
     displayTest();
 }
 
+function validateAnswer0(){
+    console.log('you have chosen A')
+    chosen.push(questions[questionIndex].choices[0]);
+    if(chosen[questionIndex]!=answer[questionIndex]){
+        score-=10;
+        wrongScreen.classList.remove("hide");
 
-for(let i=0; i < questions.length; i++) {
-   answer.push(questions[i].answer);
+    } else {
+        score += 10;
+        correctScreen.classList.remove("hide");
+    }
+    nextBtn.addEventListener ("click", nextQuestion);
 }
 
+function validateAnswer1(){
+    console.log('you have chosen B')
+    chosen.push(questions[questionIndex].choices[1]);
+    console.log(chosen);
+    if(chosen[questionIndex]!=answer[questionIndex]){
+        score-=10;
+        wrongScreen.classList.remove("hide");
+
+    } else {
+        score += 10;
+        correctScreen.classList.remove("hide");
+    }
+    nextBtn.addEventListener ("click", nextQuestion);
+}
+
+function validateAnswer2(){
+    console.log('you have chosen C')
+    chosen.push(questions[questionIndex].choices[2]);
+    if(chosen[questionIndex]!=answer[questionIndex]){
+        score-=10;
+        wrongScreen.classList.remove("hide");
+
+    } else {
+        score += 10;
+        correctScreen.classList.remove("hide");
+    }
+    nextBtn.addEventListener ("click", nextQuestion);
+}
+
+function validateAnswer3(){
+    console.log('you have chosen D')
+    chosen.push(questions[questionIndex].choices[3]);
+    if(chosen[questionIndex]!=answer[questionIndex]){
+        score-=10;
+        wrongScreen.classList.remove("hide");
+
+    } else {
+        score += 10;
+        correctScreen.classList.remove("hide");
+    }
+    nextBtn.addEventListener ("click", nextQuestion);
+}
 
 for(let i = 0; i < questionBtns.length; i++) {
     var button = questionBtns[i];
-    button.addEventListener("click", nextQuestion)
+choice0.addEventListener("click", validateAnswer0);
+choice1.addEventListener("click", validateAnswer1);
+choice2.addEventListener("click", validateAnswer2);
+choice3.addEventListener("click", validateAnswer3);
 }
